1121119b3337a4b4afcbf3282abbd3e720579050
Nallu-Rishitha <n.rishitha19@gmail.com>
2023-09-28 13:29:47 -0500

    Empty view and vertical stack layout added

--- MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -4,6 +4,8 @@ namespace MonkeyFinder.ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+    [ObservableProperty]
+    bool isRefreshing;
     public ObservableCollection<Monkey> Monkeys { get; } = new();
     MonkeyService monkeyService;
     IConnectivity connectivity;
@@ -61,6 +63,7 @@ public partial class MonkeysViewModel : BaseViewModel
         finally
         {
             IsBusy = false;
+            IsRefreshing = false;
         }
 
     }
