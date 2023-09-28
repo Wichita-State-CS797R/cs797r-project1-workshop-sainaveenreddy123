62ff890ed183bdcd80b7125b2a35429c68d980ce
sainaveenreddy123 <sainaveenreddy123@gmail.com>
2023-09-28 11:45:26 -0500

    json subscription added

--- Part 2 - MVVM/MonkeyFinder/ViewModel/BaseViewModel.cs
+++ Part 2 - MVVM/MonkeyFinder/ViewModel/BaseViewModel.cs
@@ -1,38 +1,13 @@
-﻿public class BaseViewModel : INotifyPropertyChanged
+﻿namespace MonkeyFinder.ViewModel;
+
+public partial class BaseViewModel : ObservableObject
 {
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
     bool isBusy;
+
+    [ObservableProperty]
     string title;
-    //...
-    public bool IsBusy
-    {
-        get => isBusy;
-        set
-        {
-            if (isBusy == value)
-                return;
-            isBusy = value;
-            OnPropertyChanged();
-            // Also raise the IsNotBusy property changed
-            OnPropertyChanged(nameof(IsNotBusy));
-        }
 
-    }
     public bool IsNotBusy => !IsBusy;
-    //...
-
-    public string Title
-    {
-        get => title;
-        set
-        {
-            if (title == value)
-                return;
-            title = value;
-            OnPropertyChanged();
-        }
-    }
-    //...
-    public event PropertyChangedEventHandler PropertyChanged;
-    public void OnPropertyChanged([CallerMemberName] string name = null) =>
-    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 }
---
+++ Part 2 - MVVM/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -0,0 +1,45 @@
+﻿namespace MonkeyFinder.ViewModel;
+using MonkeyFinder.Services;
+
+public partial class MonkeysViewModel : BaseViewModel
+    {
+    //...
+    [RelayCommand]
+    async Task GetMonkeysAsync()
+    {
+        if (IsBusy)
+            return;
+        //...
+        try
+        {
+            IsBusy = true;
+
+            var monkeys = await monkeyService.GetMonkeys();
+            if (Monkeys.Count != 0)
+                Monkeys.Clear();
+
+            foreach (var monkey in monkeys)
+                Monkeys.Add(monkey);
+        }
+        //... 
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Unable to get monkeys: {ex.Message}");
+            await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+    //...
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+    MonkeyService monkeyService;
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        Title = "Monkey Finder";
+        this.monkeyService = monkeyService;
+    }
+}
+
